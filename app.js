620facd3cf6edfc73a36f50d444d70db83264a7f
FabianHirjan <35564006+FabianHirjan@users.noreply.github.com>
2018-12-28 12:43:12 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var person =
+    {
+    firstname: "Test",
+    lastname: "Test",
+    age: 28
+    }
+    console.log(person);
+
+
+function sum(a,b) {
+    return a+b;
+}
+console.log(sum(7,3));
+
+var futminore = function() {
+    console.log("Nimic 1-0");
+}
+
+futminore();
+setTimeout(futminore,5000);
